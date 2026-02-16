b3c3f9b23d651ee047905990960630f131898900
adjisay <pe_1n@mail.ru>
2026-02-17 00:46:25 +0300

    completely laid out the movie page

---
+++ src/components/PersonCard/PersonCard.jsx
@@ -0,0 +1,26 @@
+import { Image } from "minista"
+import "./PersonCard.scss"
+
+const PersonCard = (props) => {
+  const { imgSrc, imgAlt, name, subtitle } = props
+  const hasBody = Boolean(name || subtitle)
+
+  return (
+    <div className="person-card">
+      <Image
+        className="person-card__image"
+        src={imgSrc}
+        alt={imgAlt}
+        title={imgAlt}
+      />
+      {hasBody && (
+        <div className="person-card__body">
+          {name && <h4 className="person-card__name">{name}</h4>}
+          {subtitle && <p className="person-card__subtitle">{subtitle}</p>}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default PersonCard
