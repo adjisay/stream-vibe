ac698fba53c7555ed06278f6134bc5c41ebd97d6
adjisay <pe_1n@mail.ru>
2026-02-17 20:10:01 +0300

    added support page

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import OverlayMenu from "@/modules/OverlayMenu"
 import SliderCollections from "@/modules/SliderCollections"
 import TabsCollection from "@/modules/TabsCollection"
 import VideoPlayerCollection from "@/modules/VideoPlayerCollection"
+import InputMaskCollection from "./modules/inputMaskCollection"
+import SelectCollection from "./modules/SelectCollection"
 
 new OverlayMenu()
 new SliderCollections()
 new TabsCollection()
 new VideoPlayerCollection()
+new InputMaskCollection()
+new SelectCollection()
