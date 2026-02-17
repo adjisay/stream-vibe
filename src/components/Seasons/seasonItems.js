e27ea09d0033f6ff2ad5ad8872443feec0c856f6
adjisay <pe_1n@mail.ru>
2026-02-18 00:31:51 +0300

    fix incorrect video path

--- src/components/Seasons/seasonItems.js
+++ src/components/Seasons/seasonItems.js
@@ -1,5 +1,5 @@
-import videoExampleSrc from '@/assets/videos/example.mp4'
-import videoPosterSrc from '@/assets/images/video-posters/1.jpg'
+import videoExampleSrc from "@/assets/videos/1.mp4"
+import videoPosterSrc from "@/assets/images/video-posters/1.jpg"
 
 const seasonItems = [
   {
@@ -13,7 +13,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -24,7 +24,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -35,7 +35,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -46,7 +46,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -57,7 +57,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -68,7 +68,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -79,7 +79,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -90,7 +90,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -101,112 +101,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-    ],
-  },
-  {
-    title: "Season 01",
-    subtitle: "9 Episodes",
-    episodes: [
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
-          poster: videoPosterSrc,
-        },
-      },
-      {
-        number: "01",
-        title: "Chapter One : The Vanishing of Will Byers",
-        description:
-          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
-        duration: "49 min",
-        video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -223,7 +118,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -234,7 +129,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -245,7 +140,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -256,7 +151,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -267,7 +162,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -278,7 +173,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -289,7 +184,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -300,7 +195,7 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
@@ -311,7 +206,112 @@ const seasonItems = [
           "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
         duration: "49 min",
         video: {
-          src: videoExampleSrc ,
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+    ],
+  },
+  {
+    title: "Season 01",
+    subtitle: "9 Episodes",
+    episodes: [
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
+          poster: videoPosterSrc,
+        },
+      },
+      {
+        number: "01",
+        title: "Chapter One : The Vanishing of Will Byers",
+        description:
+          "On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.",
+        duration: "49 min",
+        video: {
+          src: videoExampleSrc,
           poster: videoPosterSrc,
         },
       },
